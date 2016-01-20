5a19c5990968be4f763aa3eb8d2bb59d44fb6366
Matthew Meitl <DarkSamus6@gmail.com>
2016-01-20 20:44:56 +0000

    example test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,9 +9,12 @@ skip_before_filter :verify_authenticity_token, :only => :auth_callback
     end
 
     def auth_callback
-      auth = request.env['omniauth.auth']
-      session[:current_user] = { nickname: request.env["omniauth.auth"][:info][:Sinkles], image: request.env["omniauth.auth"][:info][:image], uid: request.env["omniauth.auth"][:uid] }
-      redirect_to root_url  
+        render :text => request.env["omniauth.auth"].info.to_hash.inspect
+      #auth = request.env['omniauth.auth']
+      #session[:current_user] = { :nickname => auth.info['nickname'],
+       #                                   :image => auth.info['image'],
+       #                                   :uid => auth.uid }
+      #redirect_to root_url  
     end 
     
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,5 @@ require "openid/store/filesystem"
 
 # config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :steam, ENV["STEAM_WEB_API_KEY"]
+  provider :steam, ENV["STEAM_WEB_API_KEY"], :storage => OpenID::Store::Filesystem.new("/tmp")
 end
